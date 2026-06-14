86584430b3abeb4a53ff0fad50a5da938ec873e2
Luke Waters-Thorpe <thelukethorpe@outlook.com>
2026-06-14 16:38:59 +0100

    Setup client and server apps

---
+++ src/app/client/client_main.cpp
@@ -0,0 +1,7 @@
+#include <core/log.h>
+
+int main()
+{
+    LOG_INFO("Starting client application...");
+    return 0;
+}
---
+++ src/app/server/server_main.cpp
@@ -0,0 +1,7 @@
+#include <core/log.h>
+
+int main()
+{
+    LOG_INFO("Starting server application...");
+    return 0;
+}
